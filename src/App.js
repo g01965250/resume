e03bf6d4767a5fb00417807ecb6dca0af2b61bc4
yu <g01965250@gmail.com>
2022-09-14 10:51:31 +0800

    sidebar finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
+import { FaTwitter,FaFacebookF ,FaInstagramSquare,FaPinterest,FaYoutube} from "react-icons/fa";
+import { BsFillArrowLeftSquareFill } from "react-icons/bs";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <BsFillArrowLeftSquareFill className="sidebar-arrow"/>
+        <nav className="nav-wrapper">
+          <div className="nav-title">REWALL</div>
+          <ul>
+            <li className="nav-btn nav-activ"><a href="#" >HOME</a></li>
+            <li className="nav-btn"><a href="#">ABOUT ME</a></li>
+            <li className="nav-btn"><a href="#">EDUCATION</a></li>
+            <li className="nav-btn"><a href="#">SKILL</a></li>
+            <li className="nav-btn"><a href="#">PROJECT</a></li>
+            <li className="nav-btn"><a href="#">CONTACT</a></li>
+          </ul>
+        </nav>
+        <div className="social-wrapper">
+          <div className="social">
+            <ul>
+              <li><a href="#"><FaTwitter/></a></li>
+              <li><a href="#"><FaFacebookF/></a></li>
+              <li><a href="#"><FaInstagramSquare/></a></li>
+              <li><a href="#"><FaPinterest/></a></li>
+              <li><a href="#"><FaYoutube/></a></li>
+            </ul>
+          </div>
+          <div className="desc">© 2021 Rewall Template <br/>Created by Frenify</div>
+        </div>
+      </div>
     </div>
   );
 }
