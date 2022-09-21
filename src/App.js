36b51a656f4ebbb17798c0a77b1ac60e3f0776da
yu <g01965250@gmail.com>
2022-09-21 09:56:15 +0800

    resume update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { FaTwitter,FaFacebookF ,FaInstagramSquare,FaPinterest,FaYoutube} from "react-icons/fa";
+import { FaTwitter,FaFacebookF ,FaInstagramSquare,FaGithub,FaYoutube} from "react-icons/fa";
 import { BsFillArrowLeftSquareFill,BsFillArrowDownSquareFill } from "react-icons/bs";
-import img2 from "./image/2.jpg";
-import img3 from "./image/3.jpg";
-import img4 from "./image/4.jpg";
-
 import './App.css';
 
 function App() {
@@ -17,29 +13,29 @@ function App() {
           <nav className="nav-wrapper">
             <div className="nav-title">REWALL</div>
             <ul>
-              <li className="nav-btn nav-activ"><a href="#" >HOME</a></li>
-              <li className="nav-btn"><a href="#">ABOUT ME</a></li>
-              <li className="nav-btn"><a href="#">EDUCATION</a></li>
-              <li className="nav-btn"><a href="#">SKILL</a></li>
-              <li className="nav-btn"><a href="#">PROJECT</a></li>
-              <li className="nav-btn"><a href="#">CONTACT</a></li>
+              <li className="nav-btn nav-activ"><a href="#home" >HOME</a></li>
+              <li className="nav-btn"><a href="#about">ABOUT ME</a></li>
+              <li className="nav-btn"><a href="#experience">EXPERIENCE</a></li>
+              <li className="nav-btn"><a href="#skill">SKILL</a></li>
+              <li className="nav-btn"><a href="#project">PROJECT</a></li>
+              <li className="nav-btn"><a href="#contact">CONTACT</a></li>
             </ul>
           </nav>
           <div className="social-wrapper">
             <div className="social">
               <ul>
-                <li><a href="#"><FaTwitter/></a></li>
-                <li><a href="#"><FaFacebookF/></a></li>
-                <li><a href="#"><FaInstagramSquare/></a></li>
-                <li><a href="#"><FaPinterest/></a></li>
-                <li><a href="#"><FaYoutube/></a></li>
+                <li><a href="https://twitter.com/"><FaTwitter/></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100000480666744"><FaFacebookF/></a></li>
+                <li><a href="https://www.instagram.com/g01965250/"><FaInstagramSquare/></a></li>
+                <li><a href="https://github.com/g01965250"><FaGithub/></a></li>
+                <li><a href="https://www.youtube.com/"><FaYoutube/></a></li>
               </ul>
             </div>
-            <div className="desc">© 2021 Rewall Template <br/>Created by Frenify</div>
+            {/* <div className="desc">© 2021 Rewall Template <br/>Created by Frenify</div> */}
           </div>
         </div>
         <div className="content-wrapper">
-            <div className="home-wrapper">
+            <div id="home" className="home-wrapper">
               <div className="picture-wrapper">
                 <div className="picture"></div>
               </div>
@@ -49,51 +45,51 @@ function App() {
               </div>
               <BsFillArrowDownSquareFill className="home-arrow"/>
             </div>
-            <div className="about-wrapper">
+            <div id="about" className="about-wrapper">
               <div className="about-title-wrapper">
                 <div className="about-title">ABOUT ME</div>
                 <div className="about-dsec">I'm a Freelancer Front-end Developer with over 12 years of experience. I'm from London, UK. I code and create web elements for amazing people around the world. I like work with new people. New people are new experiences.</div>
               </div>
               <ul className="about-label-wrapper">
-                <li className="about-label">Name: Felicia Amor</li>
-                <li className="about-label">Birthday: June 14, 1992</li>
+                <li className="about-label">Name: <span className="chinese">朱冠宇</span></li>
+                <li className="about-label">Birthday: July 22, 1993</li>
                 <li className="about-label">Age: 29 Years</li>
-                <li className="about-label">Adress: London, UK</li>
-                <li className="about-label">Phone: (+1) 7785 355 344</li>
-                <li className="about-label">Skype: Felicia.A</li>
-                <li className="about-label">Email: felicia@domainname.com</li>
-                <li className="about-label">Telegram: Available</li>
+                <li className="about-label">Adress:<span className="chinese">新竹,臺灣</span></li>
+                <li className="about-label">Phone: 0912-144-381</li>
+                {/* <li className="about-label">GitHub:g01965250</li> */}
+                <li className="about-label">Email: g01965250@gmail.com</li>
+              {/*   <li className="about-label">Telegram: Available</li> */}
               </ul>
               <BsFillArrowDownSquareFill className="about-arrow"/>
             </div>
-            <div className="education-wrapper">
-              <div className="education-title">EDUCATION</div>
+            <div id="experience" className="education-wrapper">
+              <div className="education-title">EXPERIENCE</div>
               <ul>
                 <li>
                   <div className="education-item">
-                    <h3>FRENIFY UNIVERSITY<span>( 2014 — 2017 )</span></h3>
-                    <h2>Computer Science</h2>
+                    <h3>明新科技大學-電子工程系<span>( 2011 — 2016 )</span></h3>
+                    {/* <h2>電子工程系</h2> */}
                     <p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                   </div>
                 </li>
                 <li>
                   <div className="education-item">
-                    <h3>EDU UNIVERSITY<span>( 2011 — 2014 )</span></h3>
-                    <h2>Master Degree</h2>
+                    <h3>鴻佰科技股份有限公司-作業員<span>( 2021 — 2022 )</span></h3>
+                    {/* <h2>Master Degree</h2> */}
                     <p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                   </div>
                 </li>
                 <li>
                   <div className="education-item">
-                    <h3>CLOLUMBIA COLLEGE<span>( 2007 — 2011 )</span></h3>
-                    <h2>Computer Science</h2>
+                    <h3>海軍陸戰隊志願役士兵<span>( 2016 — 2020 )</span></h3>
+                    {/* <h2>Computer Science</h2> */}
                     <p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                   </div>
                 </li>
               </ul>
               <BsFillArrowDownSquareFill className="education-arrow"/>
             </div>
-            <div className="skill-wrapper">
+            <div id="skill" className="skill-wrapper">
               <div className="skill-title">SKILL</div>
                 <div className="skill-bar-wrapper ">
                   <div className="bar-style92">
@@ -123,7 +119,7 @@ function App() {
                 </div>
               <BsFillArrowDownSquareFill className="skill-arrow"/>
             </div>
-            <div className="project-wrapper">
+            <div id="project" className="project-wrapper">
               <div className="project-title-wrapper">
                 <div className="project-title">PROJECT</div>
                 <div className="project-desc">Now, I’m working on a professional, visually sophisticated and technologically proficient, responsive and multi-functional wordpress theme Elonissa. If you are interested to work with me, click below button and contact with me right now, so we can talk about your project.              </div>
@@ -152,12 +148,12 @@ function App() {
               </div>
               <BsFillArrowDownSquareFill className="project-arrow"/>
             </div>
-            <div className="contact-wrapper">
+            <div id="contact" className="contact-wrapper">
                 <div className="contact-title">CONTACT</div>
               <div className="contact-input-wrapper">
                 <input className="contact-input"placeholder="Name *"></input>
                 <input className="contact-input"placeholder="Email *"></input>
-                <input className="contact-input phone"placeholder="Phione"></input>
+                <input className="contact-input phone"placeholder="Phone"></input>
                 <textarea className="contact-input message"placeholder="Message"></textarea>
                 <div className="contact-btn-wrapper">
                   <a href="#" className="contact-btn">SEND MESSAGE</a>
@@ -167,8 +163,8 @@ function App() {
                 <p>ADDRESS</p>
                 <h3>69 Queen St, London, United Kingdom</h3>
                 <p>PHONE</p>
-                <h3>(+706) 898-0751</h3>
-                <p><a href="#">frenifyteam@gmail.com</a></p>
+                <h3>0912-144-381</h3>
+                <p><a href="g01965250@gmail.com">g01965250@gmail.com</a></p>
               </div>
               <BsFillArrowDownSquareFill className="project-arrow"/>
             </div>
