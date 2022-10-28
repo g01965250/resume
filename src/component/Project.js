d06040248c75ffa30c5f6a120f11471ce036baf8
yu <g01965250@gmail.com>
2022-10-28 15:23:42 +0800

    updata project describe

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { BsFillArrowDownSquareFill } from 'react-icons/bs';
+import { BsFillArrowDownSquareFill, BsExclamationOctagon } from 'react-icons/bs';
 import todopic from '../image/todolist.jpg';
 import calculaterpic from '../image/Calculator.jpg';
 import pomodoropic from '../image/Pomodoro.jpg';
@@ -22,6 +22,15 @@ function Project({ projectAnchor, contactAnchor }) {
                 <a href="https://g01965250.github.io/calculater/">
                   <img src={calculaterpic} alt="" />
                 </a>
+                <div className="exclamationIcon-wrapper">
+                  <BsExclamationOctagon className="exclamationIcon" />
+                  <div className="project-exclamation-wrapper">
+                    <p>
+                      簡介：一個標準的計算機。
+                      原本透過畫面按鈕進行計算，後面研究透過 Rract事件監聽增加使用鍵盤控制。
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="project-list project-list-left">
@@ -29,6 +38,18 @@ function Project({ projectAnchor, contactAnchor }) {
                 <a href="https://g01965250.github.io/git-pomodoro/">
                   <img src={pomodoropic} alt="" />
                 </a>
+                <div className="exclamationIcon-wrapper">
+                  <BsExclamationOctagon className="exclamationIcon" />
+                  <div className="project-exclamation-wrapper">
+                    <p>
+                      簡介：使我們可以用最高強度專注在一件事情上25分鐘，
+                      例如看書、思考或各種工作任務；
+                      然後讓自己精神放鬆5分鐘。一直重複這個循環，可以讓自己抓緊時間並維持最高專注力。
+                      <br />
+                      使用技術：將ToDoList搭配倒數計時
+                    </p>
+                  </div>
+                </div>
               </div>
               <div className="project-list-title">番茄鐘</div>
               <div className="project-list-desc" />
@@ -40,6 +61,12 @@ function Project({ projectAnchor, contactAnchor }) {
                 <a href="https://g01965250.github.io/todolist/">
                   <img src={todopic} alt="" />
                 </a>
+                <div className="exclamationIcon-wrapper">
+                  <BsExclamationOctagon className="exclamationIcon" />
+                  <div className="project-exclamation-wrapper">
+                    <p>簡介：ToDoList作品練習</p>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="project-list project-list-left">
@@ -47,6 +74,12 @@ function Project({ projectAnchor, contactAnchor }) {
                 <a href="https://g01965250.github.io/LineWeb/">
                   <img src={linepic} alt="" />
                 </a>
+                <div className="exclamationIcon-wrapper">
+                  <BsExclamationOctagon className="exclamationIcon" />
+                  <div className="project-exclamation-wrapper">
+                    <p>簡介：使用 React框架進行切版並將內容元件化</p>
+                  </div>
+                </div>
               </div>
               <div className="project-list-title">Line切版練習</div>
               <div className="project-list-desc" />
