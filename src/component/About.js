ffe2cdc479aa7a34d500f0172ee756b2f5b07e26
yu <g01965250@gmail.com>
2022-10-31 12:06:31 +0800

    updata about describe

--- src/component/About.js
+++ src/component/About.js
@@ -9,7 +9,7 @@ function About({ aboutAnchor, experienceAnchor }) {
         <div className="about-content">
           <div className="about-title-wrapper">
             <div className="about-title title-bottomLine">ABOUT ME</div>
-            <div className="about-dsec">我是冠宇，來自桃園，現居新竹，畢業於明新科技大學電子工程系。自學前端設計半年經驗，平常會用HTML+CSS 練習刻版，JavaScript 則是上LeetCode 練習解題，React 有實作一些小專案。目前沒有相關的正職工作經驗，但我相信自己已經準備好進入該領域的職場，未來希望在短期能快速的進入工作軌道外，更能夠在公司的環境中成長，不論在前端的程式領域，或是任何面向挑戰都可以不斷的學習與精進。</div>
+            <div className="about-dsec">我是冠宇，來自桃園，現居新竹，畢業於明新科技大學電子工程系。自學前端設計半年經驗，平常會用HTML+CSS 練習刻版，JavaScript 則是上LeetCode 練習解題，React 有實作一些小專案。目前沒有相關的正職工作經驗，但我相信自己已經準備好進入該領域的職場，未來希望在短期能快速的進入工作軌道外，更能夠在公司的環境中成長，不論在前端的程式領域，或是任何面向挑戰都會不斷的學習與精進。</div>
           </div>
           <ul className="about-label-wrapper">
             <li className="about-label">
