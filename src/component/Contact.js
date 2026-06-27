2a24eaaeab99ad33067f77080ec3c7ecedead404
Orton <g01965250@gmail.com>
2026-06-27 23:27:17 +0800

    refactor(contact): replace fixed widths with flex layout and adjust form grid

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -6,75 +6,78 @@ function Contact({ contactAnchor }) {
     <div>
       <div id="contact" className="section-wrapper" ref={contactAnchor}>
         <div className="section-title title-bottomLine">CONTACT</div>
-        <form
-          id="fs-frm"
-          name="complaint-form"
-          acceptCharset="utf-8"
-          action="https://formspree.io/f/xoqbdeey"
-          method="post"
-        >
-          <fieldset id="fs-frm-inputs" className="frm-inputs">
-            <div className="input-container">
+        <div className="form-wrapper">
+          <form
+            id="fs-frm"
+            name="complaint-form"
+            acceptCharset="utf-8"
+            action="https://formspree.io/f/xjvzpnak"
+            method="post"
+          >
+            <fieldset id="fs-frm-inputs" className="frm-inputs">
+              <div className="input-container">
+                <input
+                  placeholder=" "
+                  type="text"
+                  name="name"
+                  required=""
+                  className="contact-input"
+                />
+                <span>Name *</span>
+              </div>
+              <div className="input-container">
+                <input
+                  placeholder=" "
+                  type="tel"
+                  minLength="9"
+                  maxLength="10"
+                  name="telephone"
+                  className="contact-input phone"
+                />
+                <span>Phone</span>
+              </div>
+              <div className="input-container">
+                <input
+                  placeholder=" "
+                  type="email"
+                  name="_replyto"
+                  required=""
+                  className="contact-input"
+                />
+                <span>Email *</span>
+              </div>
+              <div className="input-container">
+                <textarea
+                  name="complaint"
+                  required=""
+                  className="contact-input message"
+                  placeholder=" "
+                />
+                <span>Message</span>
+                <input
+                  type="hidden"
+                  name="_subject"
+                  id="email-subject"
+                  value="Complaint Form Submission"
+                />
+              </div>
               <input
-                placeholder=" "
-                type="text"
-                name="name"
-                required=""
-                className="contact-input"
+                type="submit"
+                value="SEND MESSAGE"
+                className="contact-btn-wrapper"
               />
-              <span>Name *</span>
-            </div>
-            <div className="input-container">
-              <input
-                placeholder=" "
-                type="email"
-                name="_replyto"
-                required=""
-                className="contact-input"
-              />
-              <span>Email *</span>
-            </div>
-            <div className="input-container">
-              <input
-                placeholder=" "
-                type="tel"
-                minLength="9"
-                maxLength="10"
-                name="telephone"
-                className="contact-input phone"
-              />
-              <span>Phone</span>
-            </div>
-            <div className="input-container">
-              <textarea
-                name="complaint"
-                required=""
-                className="contact-input message"
-                placeholder=" "
-              />
-              <span>Message</span>
-              <input
-                type="hidden"
-                name="_subject"
-                id="email-subject"
-                value="Complaint Form Submission"
-              />
-            </div>
-            <input
-              type="submit"
-              value="SEND MESSAGE"
-              className="contact-btn-wrapper"
-            />
-          </fieldset>
-        </form>
-        <div className="contact-info">
-          <p>ADDRESS</p>
-          <h3>臺灣 新竹</h3>
-          <p>PHONE</p>
-          <h3>0912-144-381</h3>
-          <p>
-            <a href="g01965250@gmail.com">g01965250@gmail.com</a>
-          </p>
+            </fieldset>
+          </form>
+
+          <div className="contact-info">
+            <p>ADDRESS</p>
+            <h3>臺灣 新竹</h3>
+            <p>PHONE</p>
+            <h3>0912-144-381</h3>
+            <p>
+              <a href="g01965250@gmail.com">g01965250@gmail.com</a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
